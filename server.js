12f8508143b223edaaadc586c9b38012d4e6696b
gkandemi <gokhan@gkandemir.com>
2017-04-13 18:45:46 +0300

    id ye göre todo elemanı getirilmesi için gerekli olan route tanımlaması yapıldı

--- server.js
+++ server.js
@@ -21,10 +21,27 @@ app.get("/", function(req, res){
 })
 
 // GET /todos
-// GET /todos/:id
-
 app.get("/todos", function(req, res){
     res.json(todos);
+})
+
+// GET /todos/:id
+app.get("/todos/:id", function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var todoFounded = false;
+
+    todos.forEach(function(todo){
+        if(todo.id === todoId){
+            todoFounded = true;
+            res.json(todo);
+        }
+
+        // Eğer herhangi bir kayıt bulunamazsa 404 durumunu gönder..
+        if(!todoFounded){
+            res.status(404).send();
+        }
+    })
+    //res.send("Asking todo with id of " + req.params.id);
 })
 
 app.listen(PORT, function(){
