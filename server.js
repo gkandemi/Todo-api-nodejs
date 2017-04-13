1d6df5555bf84fdc2c9a1d9301c76190f19d7214
gkandemi <gokhan@gkandemir.com>
2017-04-13 18:16:49 +0300

    /todos için get request yazildi..

--- server.js
+++ server.js
@@ -2,10 +2,30 @@ var express = require("express");
 var app     = express();
 var PORT    = process.env.PORT || 3000;
 
+var todos = [{
+    id : 1,
+    description : "NMX Site Table..",
+    complete : true
+},{
+    id : 2,
+    description : "Nargile İç",
+    complete : false
+},{
+    id : 3,
+    description : "Kungfuya git",
+    complete : false
+}];
+
 app.get("/", function(req, res){
     res.send("Todo API Root");
 })
 
+// GET /todos
+// GET /todos/:id
+
+app.get("/todos", function(req, res){
+    res.json(todos);
+})
 
 app.listen(PORT, function(){
     console.log("Express listening on " + PORT + " !");
