6c8bddc3f25c325f796ad8feaefb4034cf517fa6
gkandemi <gokhan@gkandemir.com>
2017-04-16 23:05:04 +0300

    pas by value örneği

---
+++ variable-playground.js
@@ -0,0 +1,14 @@
+var grades = [15,27];
+
+function addGrades(gradess){
+    
+    gradess.push(43);
+
+}
+
+addGrades(grades);
+console.log(grades);
+
+// pass value ile array ve object veri türlerinin değelerini fonksiyona gönderebilir
+// direk degerlerini degistirebiliriz.
+// diger veri türleri için boolean string, number pass by value işe yaramaz.
