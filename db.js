b4fb147cba0243c879a7a70c57cbbf0eb5737d99
gkandemi <gokhan@gkandemir.com>
2017-04-19 13:26:07 +0300

    production sistemi icin postgres eklendi

--- db.js
+++ db.js
@@ -1,8 +1,21 @@
 var Sequelize = require("sequelize");
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect' : 'sqlite',
-    'storage' : __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development'; // node çalıştığı ortama göre env değişkenini doldurur.
+// Bizim burada bunu tanımlama amacımız ise production ortamında yani heroku üzerinden calistirirken 
+// postgres local üzerinde calistirirken ise sqlite kullanıcak olmamız..
+var sequelize;
+
+// DATABASE_URL degiskeni bize heroku app içerisinde yüklediğimiz addons olan heroku-postgresql den verildi..
+// bu degisken kendi icerisinde connection string gibi verileri icermektedir. 
+if (env === "production") {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+    });
+} else {
+    sequelize = new new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
 
 var db = {};
 
